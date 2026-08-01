d117c3d1931d68adbebe42a9d9f60eda379029e7
cpfur18 <cpfur18@gmail.com>
2026-08-02 08:21:20 +0900

    feat: CustomUserDetails 기능 구현

---
+++ libs/common/src/main/java/com/logistics/common/security/CustomUserDetails.java
@@ -0,0 +1,43 @@
+package com.logistics.common.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public class CustomUserDetails implements UserDetails {
+    private final UUID userId;
+    private final String username;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UUID getUserId() { return this.userId; }
+    @Override public String getUsername() { return username; }
+    @Override public String getPassword() { return null; }
+    @Override public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    public CustomUserDetails(UUID userId, String username, Collection<? extends GrantedAuthority> authorities) {
+        this.userId = userId;
+        this.username = username;
+        this.authorities = authorities;
+    }
+
+    public static CustomUserDetails from(UUID userId, String roleStr) {
+        Collection<GrantedAuthority> authorities = Collections.singletonList(
+                new SimpleGrantedAuthority(roleStr)
+        );
+
+        return new CustomUserDetails(userId, null, authorities);
+    }
+
+    public Set<String> getRoleNames() {
+        return authorities.stream().map(GrantedAuthority::getAuthority).collect(Collectors.toSet());
+    }
+
+}
