6f014f6dbdc4cf50c4198952c137d037fd3ff934
ByeongGyu An <an622911@gmail.com>
2026-08-01 20:34:33 +0900

    feat: 애플리케이션 서비스 모듈 초기화

---
+++ apps/company-product-service/src/test/java/com/logistics/companyproductservice/CompanyProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logistics.companyproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
