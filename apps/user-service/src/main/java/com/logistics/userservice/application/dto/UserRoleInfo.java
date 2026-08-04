1ae46e998f925bbb5e234fb975be1ba158225a03
cpfur18 <cpfur18@gmail.com>
2026-08-05 07:55:05 +0900

    fix: User Entity name 필드 유니크 제약 조건 해제

--- apps/user-service/src/main/java/com/logistics/userservice/application/dto/UserRoleInfo.java
+++ apps/user-service/src/main/java/com/logistics/userservice/application/dto/UserRoleInfo.java
@@ -3,4 +3,4 @@ package com.logistics.userservice.application.dto;
 import com.logistics.userservice.domain.Role;
 import java.util.UUID;
 
-public record UserRole(UUID userId, Role role) {}
+public record UserRoleInfo(UUID userId, Role role) {}
