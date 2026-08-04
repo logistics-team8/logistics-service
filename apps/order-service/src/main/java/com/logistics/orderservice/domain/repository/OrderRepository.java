f82ce5608c4431ac36a3c1d9d4ec8b27d25960cf
grrr-h <hong303047@gmail.com>
2026-08-05 04:44:49 +0900

    feat: 주문 조회 Repository 구현

--- apps/order-service/src/main/java/com/logistics/orderservice/domain/repository/OrderRepository.java
+++ apps/order-service/src/main/java/com/logistics/orderservice/domain/repository/OrderRepository.java
@@ -1,6 +1,8 @@
 package com.logistics.orderservice.domain.repository;
 
 import com.logistics.orderservice.domain.model.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +10,11 @@ import java.util.UUID;
 
 public interface OrderRepository{
     Order save(Order order);
+
+
+    Optional<Order> findByIdAndDeletedAtIsNull(UUID orderId);
+
+    Page<Order> findAllByDeletedAtIsNull(Pageable pageable);
+
+
 }
--- apps/order-service/src/main/java/com/logistics/orderservice/infrastructure/persistence/OrderJpaRepository.java
+++ apps/order-service/src/main/java/com/logistics/orderservice/infrastructure/persistence/OrderJpaRepository.java
@@ -1,9 +1,21 @@
 package com.logistics.orderservice.infrastructure.persistence;
 
 import com.logistics.orderservice.domain.model.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface OrderJpaRepository extends JpaRepository<Order, UUID> {
+
+    Optional<Order> findByIdAndDeletedAtIsNull(
+            UUID orderId
+    );
+
+    Page<Order> findAllByDeletedAtIsNull(
+            Pageable pageable
+    );
+
 }
