b899dff4c73836a9a5a40050365886794703992e
grrr-h <hong303047@gmail.com>
2026-08-04 17:23:24 +0900

    feat: Order, OrderItem Entity 생성

---
+++ apps/order-service/src/main/java/com/logistics/orderservice/domain/model/Order.java
@@ -0,0 +1,110 @@
+package com.logistics.orderservice.domain.model;
+
+import com.logistics.common.exception.BusinessException;
+import com.logistics.orderservice.config.BaseEntity;
+import com.logistics.orderservice.error.OrderErrorCode;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "p_orders")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "order_id")
+    private UUID id;
+
+    @Column(
+            name = "order_number",
+            nullable = false,
+            unique = true,
+            length = 50
+    )
+    private String orderNumber;
+
+    @Column(name = "requester_id", nullable = false)
+    private UUID requesterId;
+
+    @Column(name = "receiver_company_id", nullable = false)
+    private UUID receiverCompanyId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false, length = 30)
+    private OrderStatus status;
+
+    @Column(name = "request_message", length = 500)
+    private String requestMessage;
+
+    @Column(name = "requested_delivery_at")
+    private LocalDateTime requestedDeliveryAt;
+
+    @OneToMany(
+            mappedBy = "order",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private final List<OrderItem> orderItems =
+            new ArrayList<>();
+
+    @Column(name = "canceled_by")
+    private UUID canceledBy;
+
+    @Column(name = "canceled_at")
+    private LocalDateTime canceledAt;
+
+
+    public static Order create(
+            String orderNumber,
+            UUID requesterId,
+            UUID receiverCompanyId,
+            String requestMessage,
+            LocalDateTime requestedDeliveryAt
+    ){
+        Order order = new Order();
+
+        order.orderNumber = orderNumber;
+        order.requesterId = requesterId;
+        order.receiverCompanyId = receiverCompanyId;
+        order.requestMessage = requestMessage;
+        order.requestedDeliveryAt = requestedDeliveryAt;
+        order.status = OrderStatus.PENDING;
+
+        return order;
+    }
+
+    public void addOrderItem(UUID productId, Integer quantity){
+        if(productId == null){
+            throw new BusinessException(OrderErrorCode.PRODUCT_ID_REQUIRED);
+        }
+
+        boolean duplicatedOrderItem = orderItems.stream()
+                .anyMatch(orderItem ->
+                        orderItem.getProductId().equals(productId)
+                );
+
+        //하나의 주문안에 같은 상품ID 중복 방지
+        if(duplicatedOrderItem){
+            throw new BusinessException(
+                    OrderErrorCode.DUPLICATE_ORDER_PRODUCT
+            );
+        }
+
+        OrderItem orderItem = OrderItem.create(
+                this,
+                productId,
+                quantity
+        );
+        this.orderItems.add(orderItem);
+    }
+
+}
