e36c8fd87c23c2614956d38d2f0971650c6cebcf
cpfur18 <cpfur18@gmail.com>
2026-08-04 15:52:14 +0900

    test: UserEntity, 승인 대기 로직 테스트

---
+++ apps/user-service/src/test/java/com/logistics/userservice/domain/UserEntityTest.java
@@ -0,0 +1,35 @@
+package com.logistics.userservice.domain;
+
+import com.logistics.common.exception.BusinessException;
+import com.logistics.userservice.application.dto.UserSignUpCommand;
+import com.logistics.userservice.presentation.dto.request.SignUpRequest;
+import com.logistics.userservice.presentation.exception.AuthErrorCode;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class UserEntityTest {
+    private User user;
+
+    @Test
+    void validateActive_success() {
+        // given
+        UserSignUpCommand command =
+                new UserSignUpCommand(
+                        "test1234",
+                        "Testtest123!",
+                        "김철수",
+                        "U123456789",
+                        null,
+                        null,
+                        Role.COMPANY_MANAGER);
+
+        user = User.create(command);
+
+        // when & then
+        assertThatThrownBy(() -> user.validateActive())
+                .isInstanceOf(BusinessException.class)
+                .hasMessage(AuthErrorCode.PENDING_APPROVAL.message());
+    }
+}
