998f3563f150d253340dbda2e6d6d9cb03e11855
cpfur18 <cpfur18@gmail.com>
2026-08-06 03:43:30 +0900

    test: 에러 핸들러, 필터 단위 테스트 작성 - TestConfig 추가 - Test를 위한 의존성 추가

---
+++ infra/gateway/src/test/java/com/logistics/gateway/presentation/exception/GatewayExceptionHandlerTest.java
@@ -0,0 +1,80 @@
+package com.logistics.gateway.presentation.exception;
+
+import com.logistics.gateway.config.TestFilterConfig;
+import com.logistics.gateway.config.TestRouteConfig;
+import com.logistics.gateway.presentation.error.GatewayErrorCode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@Import({TestFilterConfig.class, TestRouteConfig.class})
+@DisplayName("게이트웨이 예외 처리 테스트")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class GatewayExceptionHandlerTest {
+
+    private WebTestClient webTestClient;
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        webTestClient = WebTestClient
+                .bindToServer()
+                .baseUrl("http://localhost:" + port)
+                .build();
+    }
+
+    @Test
+    void handle_businessException() {
+        // when & then
+        webTestClient.get()
+                .uri("/business-exception")
+                .exchange()
+                .expectStatus().isEqualTo(GatewayErrorCode.TOKEN_EXPIRED.status())
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.error.code");
+    }
+
+    @Test
+    void handle_responseStatusException_notFound() {
+        // when & then
+        webTestClient.get()
+                .uri("/not-found")
+                .exchange()
+                .expectStatus().isEqualTo(GatewayErrorCode.RESOURCE_NOT_FOUND.status())
+                .expectBody()
+                .jsonPath("$.error.code")
+                .isEqualTo(GatewayErrorCode.RESOURCE_NOT_FOUND.code());
+    }
+
+    @Test
+    void handle_responseStatusException_badRequest() {
+        // when & then
+        webTestClient.get()
+                .uri("/bad-request")
+                .exchange()
+                .expectStatus().isEqualTo(GatewayErrorCode.INVALID_INPUT.status())
+                .expectBody()
+                .jsonPath("$.error.code")
+                .isEqualTo(GatewayErrorCode.INVALID_INPUT.code());
+    }
+
+    @Test
+    void handle_internalServerError() {
+        // when & then
+        webTestClient.get()
+                .uri("/internal-server-error")
+                .exchange()
+                .expectStatus().isEqualTo(GatewayErrorCode.INTERNAL_SERVER_ERROR.status())
+                .expectBody()
+                .jsonPath("$.error.code")
+                .isEqualTo(GatewayErrorCode.INTERNAL_SERVER_ERROR.code());
+    }
+}
