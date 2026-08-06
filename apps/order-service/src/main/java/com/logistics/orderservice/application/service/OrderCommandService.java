5e7bb61bd59102358748138fd7e75d234ae125ca
grrr-h <hong303047@gmail.com>
2026-08-06 12:51:04 +0900

    feat: service에 수정, 삭제 메서드 구현

--- apps/order-service/src/main/java/com/logistics/orderservice/application/service/OrderCommandService.java
+++ apps/order-service/src/main/java/com/logistics/orderservice/application/service/OrderCommandService.java
@@ -1,10 +1,15 @@
 package com.logistics.orderservice.application.service;
 
+import com.logistics.common.exception.BusinessException;
 import com.logistics.orderservice.application.command.CreateOrderCommand;
 import com.logistics.orderservice.application.command.CreateOrderItemCommand;
+import com.logistics.orderservice.application.command.UpdateOrderCommand;
 import com.logistics.orderservice.domain.model.Order;
 import com.logistics.orderservice.domain.repository.OrderRepository;
+import com.logistics.orderservice.error.OrderErrorCode;
 import com.logistics.orderservice.presentation.dto.response.CreateOrderResponse;
+import com.logistics.orderservice.presentation.dto.response.DeleteOrderResponse;
+import com.logistics.orderservice.presentation.dto.response.UpdateOrderResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -13,7 +18,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.UUID;
-import java.util.concurrent.ThreadLocalRandom;
 
 @Slf4j
 @Service
@@ -57,5 +61,58 @@ public class OrderCommandService {
                 .toUpperCase();
 
         return "ORD-" + date + "-" + suffix;
+    }
+
+
+
+
+    /**
+     * 주문 수정
+     * 현재는 인증 및 권한 검사 없이 기본 수정 기능만 구현
+     * 수정 가능 상태: PENDING
+     * Security 적용 후:
+     * - MASTER: 모든 주문 수정
+     * - HUB_MANAGER: 담당 허브의 주문만 수정 가능
+     */
+    @Transactional
+    public UpdateOrderResponse updateOrder(UUID userId, UpdateOrderCommand command, UUID orderId) {
+        //user의 role을 확인하고 수정할 권한과 범위를 체크해야한다.
+
+
+        Order order = orderRepository
+                .findByIdAndDeletedAtIsNull(orderId)
+                .orElseThrow(() ->
+                    new BusinessException(OrderErrorCode.ORDER_NOT_FOUND)
+                );
+
+        order.update(
+                command.requestMessage(),
+                command.requestDeliveryAt()
+        );
+
+        return UpdateOrderResponse.from(order);
+    }
+
+
+    /**
+     * 주문 논리 삭제
+     * 현재는 인증 및 권한 검사 없이 기본 삭제 기능만 구현
+     * 삭제 가능 상태: FAILED, CANCELED
+     * Security 적용 후:
+     * - MASTER: 모든 주문 삭제
+     * - HUB_MANAGER: 담당 허브의 주문 삭제
+     */
+    @Transactional
+    public DeleteOrderResponse deleteOrder(UUID userId, UUID orderId) {
+        //user의 role을 확인하고 삭제할 권한과 범위를 체크해야한다.
+
+        Order order = orderRepository
+                .findByIdAndDeletedAtIsNull(orderId)
+                .orElseThrow(() ->
+                        new BusinessException(OrderErrorCode.ORDER_NOT_FOUND)
+                );
+
+        order.delete(userId);
+        return DeleteOrderResponse.from(order);
     }
 }
