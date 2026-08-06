4e24f595fa67f14608f62513674d6d77c85a667f
cpfur18 <cpfur18@gmail.com>
2026-08-07 05:25:47 +0900

    feat: AuthService 예외 검증 로직 추가

--- apps/user-service/src/main/java/com/logistics/userservice/application/AuthService.java
+++ apps/user-service/src/main/java/com/logistics/userservice/application/AuthService.java
@@ -12,18 +12,19 @@ import com.logistics.userservice.domain.UserRepository;
 import com.logistics.userservice.domain.redis.RefreshTokenRepository;
 import com.logistics.userservice.domain.redis.RoleCacheRepository;
 import com.logistics.userservice.presentation.exception.AuthErrorCode;
+import com.logistics.userservice.presentation.exception.UserErrorCode;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class AuthService {
     private final PasswordEncoder passwordEncoder;
@@ -34,9 +35,11 @@ public class AuthService {
 
     /**
      * 로그인 요청이 들어왔을 시 User 검증 후 액세스 토큰과 리프레시 토큰 발급
+     *
      * @param command Login 정보가 담긴 DTO
      * @return Token값이 담긴 TokenResult DTO 반환
      */
+    @Transactional(readOnly = true)
     public TokenResult login(UserLoginCommand command) {
         User user =
                 userRepository
@@ -59,38 +62,55 @@ public class AuthService {
 
     /**
      * 로그아웃 요청이 들어왔을 시 Redis에서 Refresh Token과 Role 캐싱 삭제
+     *
      * @param accessToken
      */
     public void logout(String accessToken) {
         try {
             TokenPayload tokenPayload = tokenProvider.getAllClaimsFromAccessToken(accessToken);
-
-            refreshTokenRepository.delete(tokenPayload.userId());
-            roleCacheRepository.delete(tokenPayload.userId());
+            deleteUserDataFromRedis(tokenPayload.userId());
 
         } catch (ExpiredJwtException e) {
-            throw new BusinessException(AuthErrorCode.TOKEN_EXPIRED);
-        } catch (JwtException e) {
+            log.info("[Expired] 만료된 Access Token으로 로그아웃 ");
+            UUID userId = UUID.fromString(e.getClaims().getSubject());
+            deleteUserDataFromRedis(userId);
+
+        } catch (JwtException | IllegalArgumentException e) {
             throw new BusinessException(AuthErrorCode.TOKEN_INVALID);
         }
     }
 
     /**
      * Token 재발급 기존의 Refresh Token을 제거한 뒤 새로운 토큰을 재발급
+     *
      * @param refreshToken
      * @return Token값이 담긴 TokenResult DTO 반환
      */
+    @Transactional(readOnly = true)
     public TokenResult reissue(String refreshToken) {
         try {
             TokenPayload tokenPayload = tokenProvider.getAllClaimsFromRefreshToken(refreshToken);
             UUID userId = tokenPayload.userId();
+            String savedRefreshToken;
+
+            try {
+                savedRefreshToken = refreshTokenRepository.findByUserId(tokenPayload.userId());
+
+            } catch (DataAccessException e) {
+                log.error("[ERROR] Refresh Token 조회 실패 userId = {}", userId, e);
+                throw new BusinessException(CommonErrorCode.INTERNAL_SERVER_ERROR);
+            }
+
+            validateRefreshToken(refreshToken, savedRefreshToken);
 
             User user =
                     userRepository
                             .findByIdAndDeletedAtIsNull(userId)
-                            .orElseThrow(() -> new BusinessException(AuthErrorCode.INVALID_LOGIN));
+                            .orElseThrow(() -> new BusinessException(UserErrorCode.USER_NOT_FOUND));
+
             TokenClaims tokenClaims =
                     new TokenClaims(user.getId(), user.getHubId(), user.getCompanyId());
+
             // TODO : 새로운 세션이 아닌 기존 사용자가 재발급 하므로 세션 ID 유지
             UUID sessionId = tokenPayload.sessionId();
 
@@ -98,6 +118,7 @@ public class AuthService {
 
         } catch (ExpiredJwtException e) {
             throw new BusinessException(AuthErrorCode.TOKEN_EXPIRED);
+
         } catch (JwtException | IllegalArgumentException e) {
             throw new BusinessException(AuthErrorCode.TOKEN_INVALID);
         }
@@ -109,7 +130,7 @@ public class AuthService {
      * @param tokenClaims 토큰 클레임 DTO
      * @param sessionId 사용자 식별 세션 ID
      * @param role 사용자 권한
-     * @returnTokenResult (AccessToken, RefreshToken)
+     * @return TokenResult (AccessToken, RefreshToken)
      */
     private TokenResult createAuthResponse(TokenClaims tokenClaims, UUID sessionId, String role) {
         UUID userId = tokenClaims.userId();
@@ -120,16 +141,42 @@ public class AuthService {
 
         try {
             refreshTokenRepository.save(userId, refreshToken);
-        } catch (Exception e) {
+        } catch (DataAccessException e) {
             log.error("[ERROR] Refresh Token 저장 실패 userId = {}", userId, e);
             throw new BusinessException(CommonErrorCode.INTERNAL_SERVER_ERROR);
         }
 
         try {
             roleCacheRepository.save(userId, role);
-        } catch (Exception e) {
-            log.error("[Cache] Redis Role Cache 저장 실패 userId = {}", userId, e);
+        } catch (DataAccessException e) {
+            log.warn("[Cache] Role Cache 저장 실패 userId = {}", userId, e);
         }
         return new TokenResult(accessToken, refreshToken);
+    }
+
+    /**
+     * 리프레시 토큰이 일치하는지 확인
+     *
+     * @param refreshToken
+     * @param savedToken
+     */
+    private void validateRefreshToken(String refreshToken, String savedToken) {
+        if (!refreshToken.equals(savedToken)) {
+            throw new BusinessException(AuthErrorCode.TOKEN_INVALID);
+        }
+    }
+
+    private void deleteUserDataFromRedis(UUID userId) {
+        try {
+            refreshTokenRepository.delete(userId);
+        } catch (DataAccessException e) {
+            log.error("[ERROR] Refresh Token 삭제 실패 userId = {}", userId, e);
+            throw new BusinessException(CommonErrorCode.INTERNAL_SERVER_ERROR);
+        }
+        try {
+            roleCacheRepository.delete(userId);
+        } catch (DataAccessException e) {
+            log.warn("[Cache] Role Cache 삭제 실패 userId = {}", userId, e);
+        }
     }
 }
