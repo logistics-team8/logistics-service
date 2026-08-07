03a4030396bdd210011afe34548bc52bf9a6c202
grrr-h <hong303047@gmail.com>
2026-08-07 15:32:22 +0900

    fix: TimeConfig -> infrastructure.config

--- apps/order-service/src/main/java/com/logistics/orderservice/infrastructure/config/TimeConfig.java
+++ apps/order-service/src/main/java/com/logistics/orderservice/infrastructure/config/TimeConfig.java
@@ -1,4 +1,4 @@
-package com.logistics.orderservice.config;
+package com.logistics.orderservice.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
