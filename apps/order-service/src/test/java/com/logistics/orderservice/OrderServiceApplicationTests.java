ee294d08f9ef318116ab42f8aa513ccb10f9c7cc
grrr-h <hong303047@gmail.com>
2026-08-09 05:24:24 +0900

    fix: 임시 테스트를 위해 수정

--- apps/order-service/src/test/java/com/logistics/orderservice/OrderServiceApplicationTests.java
+++ apps/order-service/src/test/java/com/logistics/orderservice/OrderServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.logistics.orderservice;
 
+import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,8 +9,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 class OrderServiceApplicationTests {
 
     @Test
-    @DisplayName("주문 서비스 애플리케이션 진입점이 존재한다")
     void applicationEntryPointExists() {
-        assertThat(OrderServiceApplication.class).isNotNull();
+        AssertionsForClassTypes.assertThat(OrderServiceApplication.class)
+                .isNotNull();
     }
+
 }
