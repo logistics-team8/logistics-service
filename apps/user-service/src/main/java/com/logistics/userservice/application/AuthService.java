b1487d72eb9d9a3208c06276e2b4bfbfa883dfaa
cpfur18 <cpfur18@gmail.com>
2026-08-12 10:51:41 +0900

    refactor: 블랙리스트 도입으로 인한 Controller, Serivce 로직 변경

--- apps/user-service/src/main/java/com/logistics/userservice/application/AuthService.java
+++ apps/user-service/src/main/java/com/logistics/userservice/application/AuthService.java
@@ -7,12 +7,16 @@ import com.logistics.userservice.application.token.TokenClaims;
 import com.logistics.userservice.application.token.TokenPayload;
 import com.logistics.userservice.application.token.TokenProvider;
 import com.logistics.userservice.application.token.TokenResult;
+import com.logistics.userservice.domain.Role;
 import com.logistics.userservice.domain.User;
 import com.logistics.userservice.domain.UserRepository;
 import com.logistics.userservice.domain.redis.RefreshTokenRepository;
 import com.logistics.userservice.domain.redis.RoleCacheRepository;
+import com.logistics.userservice.domain.redis.SessionRepository;
 import com.logistics.userservice.error.AuthErrorCode;
+import com.logistics.userservice.error.ClientErrorCode;
 import com.logistics.userservice.error.UserErrorCode;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import java.util.Objects;
@@ -29,11 +33,12 @@ import org.springframework.util.StringUtils;
 @Service
 @RequiredArgsConstructor
 public class AuthService {
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final RoleCacheRepository roleCacheRepository;
+    private final SessionRepository sessionRepository;
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final TokenProvider tokenProvider;
-    private final RefreshTokenRepository refreshTokenRepository;
-    private final RoleCacheRepository roleCacheRepository;
 
     /**
      * 로그인 요청이 들어왔을 시 User 검증 후 액세스 토큰과 리프레시 토큰 발급
@@ -53,12 +58,12 @@ public class AuthService {
         }
 
         // TODO : 사용자 상태 검증 로직 (PENDING, REJECTED)
-        // user.validateActive();
+        user.validateActive();
 
         TokenClaims tokenClaims =
                 new TokenClaims(user.getId(), user.getHubId(), user.getCompanyId());
 
-        return createAuthResponse(tokenClaims, UUID.randomUUID(), user.getRole().name());
+        return createAuthResponse(tokenClaims, UUID.randomUUID(), user.getRole());
     }
 
     /**
@@ -69,12 +74,15 @@ public class AuthService {
     public void logout(String accessToken) {
         try {
             TokenPayload tokenPayload = tokenProvider.getAllClaimsFromAccessToken(accessToken);
-            deleteUserDataFromRedis(tokenPayload.userId());
-
+            deleteUserDataFromRedis(tokenPayload.userId(), tokenPayload.sessionId());
         } catch (ExpiredJwtException e) {
-            log.info("[Expired] 만료된 Access Token으로 로그아웃 ");
-            UUID userId = UUID.fromString(e.getClaims().getSubject());
-            deleteUserDataFromRedis(userId);
+            Claims claims = e.getClaims();
+
+            log.info("[EXPIRED] 만료된 Access Token으로 로그아웃 ");
+            UUID userId = UUID.fromString(claims.getSubject());
+            UUID sessionId = UUID.fromString(claims.get("sessionId", String.class));
+
+            deleteUserDataFromRedis(userId, sessionId);
 
         } catch (JwtException | IllegalArgumentException e) {
             throw new BusinessException(AuthErrorCode.TOKEN_INVALID);
@@ -91,16 +99,27 @@ public class AuthService {
         try {
             TokenPayload tokenPayload = tokenProvider.getAllClaimsFromRefreshToken(refreshToken);
             UUID userId = tokenPayload.userId();
+
+            // 새로운 세션이 아닌 기존 사용자가 재발급 하므로 세션 ID 유지
+            UUID sessionId = tokenPayload.sessionId();
+
             String savedRefreshToken;
+            boolean activeSession;
 
             try {
-                savedRefreshToken = refreshTokenRepository.findByUserId(userId).orElse(null);
+                savedRefreshToken =
+                        refreshTokenRepository.findByUserId(userId, sessionId).orElse(null);
+                activeSession = sessionRepository.exists(userId, sessionId);
             } catch (DataAccessException e) {
-                log.error("[ERROR] Refresh Token 조회 실패 userId = {}", userId, e);
-                throw new BusinessException(CommonErrorCode.INTERNAL_SERVER_ERROR);
+                log.error("[ERROR] 인증 세션 조회 실패 userId = {}", userId, e);
+                throw new BusinessException(ClientErrorCode.SERVICE_UNAVAILABLE);
             }
 
-            validateRefreshToken(refreshToken, savedRefreshToken);
+            if (!activeSession) {
+                throw new BusinessException(AuthErrorCode.TOKEN_INVALID);
+            }
+
+            validateRefreshToken(refreshToken, savedRefreshToken, userId, sessionId);
 
             User user =
                     userRepository
@@ -110,10 +129,7 @@ public class AuthService {
             TokenClaims tokenClaims =
                     new TokenClaims(user.getId(), user.getHubId(), user.getCompanyId());
 
-            // 새로운 세션이 아닌 기존 사용자가 재발급 하므로 세션 ID 유지
-            UUID sessionId = tokenPayload.sessionId();
-
-            return createAuthResponse(tokenClaims, sessionId, user.getRole().name());
+            return createAuthResponse(tokenClaims, sessionId, user.getRole());
 
         } catch (ExpiredJwtException e) {
             throw new BusinessException(AuthErrorCode.TOKEN_EXPIRED);
@@ -124,42 +140,56 @@ public class AuthService {
     }
 
     /**
-     * 액세스 토큰, 리프래시 토큰 생성 후 Redis에 Refresh Token을 저장한 뒤 TokenResult 반환
+     * 액세스 토큰, 리프래시 토큰 생성 후 Redis에 Session, Refresh Token을 저장한 뒤 TokenResult 반환
      *
      * @param tokenClaims 토큰 클레임 DTO
      * @param sessionId 사용자 식별 세션 ID
      * @param role 사용자 권한
      * @return TokenResult (AccessToken, RefreshToken)
      */
-    private TokenResult createAuthResponse(TokenClaims tokenClaims, UUID sessionId, String role) {
+    private TokenResult createAuthResponse(TokenClaims tokenClaims, UUID sessionId, Role role) {
         UUID userId = tokenClaims.userId();
 
         String accessToken = tokenProvider.generateAccessToken(tokenClaims, sessionId);
         String refreshToken = tokenProvider.generateRefreshToken(tokenClaims, sessionId);
 
         try {
-            refreshTokenRepository.save(userId, refreshToken);
+            int maxSessionSize = 1;
+
+            if (Role.DELIVERY_MANAGER == role) {
+                maxSessionSize = 5;
+            }
+
+            // 세션 저장
+            sessionRepository.save(tokenClaims.userId(), sessionId, maxSessionSize);
+
+            // 리프레시 토큰 저장
+            refreshTokenRepository.save(userId, sessionId, refreshToken);
         } catch (DataAccessException e) {
-            log.error("[ERROR] Refresh Token 저장 실패 userId = {}", userId, e);
+            log.error("[ERROR] 인증 세션 저장 실패 userId = {}", userId, e);
             throw new BusinessException(CommonErrorCode.INTERNAL_SERVER_ERROR);
         }
 
         try {
-            roleCacheRepository.save(userId, role);
+            roleCacheRepository.save(userId, role.name());
         } catch (DataAccessException e) {
-            log.warn("[Cache] Role Cache 저장 실패 userId = {}", userId, e);
+            log.warn("[CACHE] Role Cache 저장 실패 userId = {}", userId, e);
         }
         return new TokenResult(accessToken, refreshToken);
     }
 
     /**
-     * 리프레시 토큰이 일치하는지 확인
+     * 리프레시 토큰이 일치하는지 확인 일치하지 않을 시 리프레시 토큰 폐기
      *
      * @param refreshToken
      * @param savedToken
      */
-    private void validateRefreshToken(String refreshToken, String savedToken) {
+    private void validateRefreshToken(
+            String refreshToken, String savedToken, UUID userId, UUID sessionId) {
         if (!StringUtils.hasText(savedToken) || !Objects.equals(refreshToken, savedToken)) {
+            deleteUserDataFromRedis(userId, sessionId);
+            log.warn("[WARN] 유효하지 않은 토큰으로 재발급 시도 userId = {}, sessionId = {}", userId, sessionId);
+
             throw new BusinessException(AuthErrorCode.TOKEN_INVALID);
         }
     }
@@ -169,17 +199,18 @@ public class AuthService {
      *
      * @param userId
      */
-    private void deleteUserDataFromRedis(UUID userId) {
+    private void deleteUserDataFromRedis(UUID userId, UUID sessionId) {
         try {
-            refreshTokenRepository.delete(userId);
+            refreshTokenRepository.delete(userId, sessionId);
+            sessionRepository.delete(userId, sessionId);
         } catch (DataAccessException e) {
-            log.error("[ERROR] Refresh Token 삭제 실패 userId = {}", userId, e);
+            log.error("[ERROR] 인증 세션 삭제 실패 userId = {}", userId, e);
             throw new BusinessException(CommonErrorCode.INTERNAL_SERVER_ERROR);
         }
         try {
             roleCacheRepository.delete(userId);
         } catch (DataAccessException e) {
-            log.warn("[Cache] Role Cache 삭제 실패 userId = {}", userId, e);
+            log.warn("[CACHE] Role Cache 삭제 실패 userId = {}", userId, e);
         }
     }
 }
--- apps/user-service/src/main/java/com/logistics/userservice/presentation/AuthController.java
+++ apps/user-service/src/main/java/com/logistics/userservice/presentation/AuthController.java
@@ -23,8 +23,8 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
 public class AuthController implements AuthApi {
-    private final AuthService authService;
     private final CookieProvider cookieProvider;
+    private final AuthService authService;
 
     @PostMapping("/login")
     public ResponseEntity<ApiResponse<TokenResponse>> login(
