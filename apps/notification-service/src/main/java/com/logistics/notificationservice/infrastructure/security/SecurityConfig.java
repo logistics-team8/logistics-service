ef6fb2b762d77457857d8586e27f7801fbe88fc3
SiruDuck <potato32122@gmail.com>
2026-08-12 23:34:55 +0900

    refactor: User Service 호출로 Slack Id 불러오기

--- apps/notification-service/src/main/java/com/logistics/notificationservice/infrastructure/security/SecurityConfig.java
+++ apps/notification-service/src/main/java/com/logistics/notificationservice/infrastructure/security/SecurityConfig.java
@@ -2,8 +2,12 @@ package com.logistics.notificationservice.infrastructure.security;
 
 
 import com.logistics.common.security.filter.UserContextFilter;
+import com.logistics.common.security.hendler.CustomAccessDeniedHandler;
+import com.logistics.common.security.hendler.CustomAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,6 +15,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import tools.jackson.databind.json.JsonMapper;
 
 @Configuration
 @EnableWebSecurity
@@ -20,7 +25,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(
             HttpSecurity http,
-            UserContextFilter userContextFilter
+            UserContextFilter userContextFilter,
+            CustomAccessDeniedHandler customAccessDeniedHandler,
+            CustomAuthenticationEntryPoint customAuthenticationEntryPoint
     ) throws Exception {
 
         http
@@ -32,10 +39,16 @@ public class SecurityConfig {
                         )
                 )
 
+
+
                 .addFilterBefore(
                         userContextFilter,
                         UsernamePasswordAuthenticationFilter.class
                 )
+
+                .exceptionHandling(config ->
+                        config.authenticationEntryPoint(customAuthenticationEntryPoint)
+                                .accessDeniedHandler(customAccessDeniedHandler))
 
                 .authorizeHttpRequests(auth ->
                         auth
@@ -54,8 +67,29 @@ public class SecurityConfig {
         return http.build();
     }
 
+    // Bean 설정
+    @Bean
+    public CustomAccessDeniedHandler customAccessDeniedHandler(JsonMapper jsonMapper) {
+        return new CustomAccessDeniedHandler(jsonMapper);
+    }
+
+
+    @Bean
+    public CustomAuthenticationEntryPoint customAuthenticationEntryPoint(JsonMapper jsonMapper) {
+        return new CustomAuthenticationEntryPoint(jsonMapper);
+    }
+
     @Bean
     public UserContextFilter userContextFilter() {
         return new UserContextFilter();
+    }
+    @Bean
+    public RoleHierarchy roleHierarchy() {
+        return RoleHierarchyImpl.fromHierarchy(
+                """
+                ROLE_MASTER > ROLE_HUB_MANAGER
+                ROLE_HUB_MANAGER > ROLE_COMPANY_MANAGER
+                ROLE_HUB_MANAGER > ROLE_DELIVERY_MANAGER
+                """);
     }
 }
