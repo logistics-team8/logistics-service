c0f8cb66e19b10870c5690f93ca71c491b61381d
cpfur18 <cpfur18@gmail.com>
2026-08-12 10:49:49 +0900

    refactor: QueryDSL 가독성 및 회원가입 승인 시 조회를 위한 메서드 분리

--- apps/user-service/src/main/java/com/logistics/userservice/infrastructure/querydsl/AdminUserQueryRepositoryImpl.java
+++ apps/user-service/src/main/java/com/logistics/userservice/infrastructure/querydsl/AdminUserQueryRepositoryImpl.java
@@ -5,14 +5,18 @@ import com.logistics.userservice.application.dto.user.UserContext;
 import com.logistics.userservice.application.port.AdminUserQueryRepository;
 import com.logistics.userservice.domain.QUser;
 import com.logistics.userservice.domain.User;
+import com.logistics.userservice.domain.UserStatus;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -20,51 +24,88 @@ public class AdminUserQueryRepositoryImpl implements AdminUserQueryRepository {
     private static final QUser user = QUser.user;
     private final JPAQueryFactory queryFactory;
 
+    /**
+     * 관리자 회원 조회
+     *
+     * @param userContext
+     * @param searchUsersQuery
+     * @param pageable
+     * @return
+     */
     @Override
     public Page<User> searchUsers(
             UserContext userContext, SearchUsersQuery searchUsersQuery, Pageable pageable) {
-        OrderSpecifier<?>[] order = QuerydslUtils.getSort(pageable, user);
+        BooleanBuilder builder = commonBuilder(userContext, searchUsersQuery)
+                .and(QuerydslUtils.eq(user.userStatus, searchUsersQuery.userStatus()));
 
-        List<User> result =
-                queryFactory
-                        .selectFrom(user)
-                        .where(
-                                QuerydslUtils.eq(
-                                        user.hubId,
-                                        userContext.isHubManager()
-                                                ? userContext.hubId()
-                                                : searchUsersQuery.hubId()),
-                                QuerydslUtils.eq(user.userStatus, searchUsersQuery.userStatus()),
-                                QuerydslUtils.startsWith(
-                                        user.username, searchUsersQuery.username()),
-                                QuerydslUtils.eq(user.companyId, searchUsersQuery.companyId()),
-                                QuerydslUtils.eq(user.role, searchUsersQuery.role()))
-                        .offset(pageable.getOffset())
-                        .limit(pageable.getPageSize())
-                        .orderBy(order)
-                        .fetch();
+        return commonQuery(builder, pageable);
+    }
 
-        if (result.size() < pageable.getPageSize() && pageable.getOffset() == 0) {
-            return new PageImpl<>(result, pageable, result.size());
+    /**
+     * 승인 유저 조회
+     *
+     * @param userContext
+     * @param searchUsersQuery
+     * @param pageable
+     * @return
+     */
+    @Override
+    public Page<User> searchPendingUsers(UserContext userContext, SearchUsersQuery searchUsersQuery, Pageable pageable) {
+        // PENDING 상태 고정
+        BooleanBuilder builder = commonBuilder(userContext, searchUsersQuery)
+                .and(user.userStatus.eq(UserStatus.PENDING));
+
+        return commonQuery(builder, pageable);
+    }
+
+    /**
+     * 공통 쿼리 조건 빌더
+     *
+     * @param userContext 검색자 정보
+     * @param searchUsersQuery 쿼리 조건
+     * @return BooleanBuilder
+     */
+    private BooleanBuilder commonBuilder(UserContext userContext, SearchUsersQuery searchUsersQuery) {
+        BooleanBuilder builder = new BooleanBuilder();
+
+        // 허브 매니저인 경우 본인 허브 요청만 조회 가능
+        if (userContext.isHubManager()) {
+            builder.and(QuerydslUtils.eq(user.hubId, userContext.hubId()));
+        } else {
+            builder.and(QuerydslUtils.eq(user.hubId, searchUsersQuery.hubId()));
         }
 
-        Long total =
-                queryFactory
-                        .select(user.count())
-                        .from(user)
-                        .where(
-                                QuerydslUtils.eq(
-                                        user.hubId,
-                                        userContext.isHubManager()
-                                                ? userContext.hubId()
-                                                : searchUsersQuery.hubId()),
-                                QuerydslUtils.eq(user.userStatus, searchUsersQuery.userStatus()),
-                                QuerydslUtils.startsWith(
-                                        user.username, searchUsersQuery.username()),
-                                QuerydslUtils.eq(user.companyId, searchUsersQuery.companyId()),
-                                QuerydslUtils.eq(user.role, searchUsersQuery.role()))
-                        .fetchOne();
+        // 공통 쿼리
+        return builder
+                .and(QuerydslUtils.startsWith(user.username, searchUsersQuery.username()))
+                .and(QuerydslUtils.startsWith(user.name, searchUsersQuery.name()))
+                .and(QuerydslUtils.eq(user.companyId, searchUsersQuery.companyId()))
+                .and(QuerydslUtils.eq(user.role, searchUsersQuery.role()));
+    }
 
-        return new PageImpl<>(result, pageable, total == null ? 0 : total);
+    /**
+     * DB 조회 및 페이징 처리
+     *
+     * @param builder
+     * @param pageable
+     * @return
+     */
+    private Page<User> commonQuery(BooleanBuilder builder, Pageable pageable) {
+        OrderSpecifier<?>[] order = QuerydslUtils.getSort(pageable, user);
+
+        List<User> result = queryFactory
+                .selectFrom(user)
+                .where(builder)
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .orderBy(order)
+                .fetch();
+
+        JPAQuery<Long> countQuery = queryFactory
+                .select(user.count())
+                .from(user)
+                .where(builder);
+
+        return PageableExecutionUtils.getPage(result, pageable, countQuery::fetchOne);
     }
 }
